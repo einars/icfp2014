b694e3e320ddbf4286c798ad19c16f42a55b742c
arturs <arturs@fern>
2014-07-25 13:05:20 +0300

    function applicator

--- compile.lisp
+++ compile.lisp
@@ -1,3 +1,5 @@
+(defvar *linker-symbols*)
+
 (defun el-num (var frame)
   (- (length frame) (length (member var frame))))
 
@@ -6,7 +8,33 @@
 	((member var (first env)) `(ld ,depth ,(el-num var (first env))))
 	(t (find-symbol-lm var (rest env) (1+ depth)))))
 
+(defun is-arithmetic (exp)
+  (member (first exp) '(+ - / *)))
+
+(defun get-arithmetic-opcode (op)
+  (list (list (cdr (assoc op '((+ . add) (- . sub) (* . mul) (/ . div)))))))
+
+(defun compile-arithmetic (exp env)
+  (append (compile-lm (second exp) env)
+	  (compile-lm (third exp) env)
+	  (get-arithmetic-opcode (first exp))))
+
+(defun is-application (exp)
+  (and (consp exp) (symbolp (first exp))))
+
+(defun compile-list (exp-list env)
+  (cond ((null exp-list) nil)
+	(t (append (compile-lm (first exp-list) env)
+		   (compile-list (rest exp-list) env)))))
+
+(defun apply-defined (exp env)
+  (append (compile-list (rest exp) env)
+	  `((ldf ,(first exp))
+	    (ap ,(length (rest exp))))))
+
 (defun compile-lm (exp env)
   (cond ((numberp exp) `((ldc ,exp)))
 	((symbolp exp) (list (find-symbol-lm exp env)))
-	(t (error "bad expression"))))
+	((is-arithmetic exp) (compile-arithmetic exp env))
+	((is-application exp) (apply-defined exp env))
+	(t (error "bad expression ~A" exp))))
