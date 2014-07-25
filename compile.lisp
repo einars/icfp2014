07351fdc793ed44340301035202ff42b42590965
arturs <arturs@fern>
2014-07-25 12:35:32 +0300

    compile variable

--- compile.lisp
+++ compile.lisp
@@ -1,2 +1,12 @@
+(defun el-num (var frame)
+  (- (length frame) (length (member var frame))))
+
+(defun find-symbol-lm (var env &optional (depth 0))
+  (cond ((null env) (error "unknown variable ~A" var))
+	((member var (first env)) `(ld ,depth ,(el-num var (first env))))
+	(t (find-symbol-lm var (rest env) (1+ depth)))))
+
 (defun compile-lm (exp env)
-  (cond ((numberp exp) `((ldc ,exp)))))
+  (cond ((numberp exp) `((ldc ,exp)))
+	((symbolp exp) (list (find-symbol-lm exp env)))
+	(t (error "bad expression"))))
