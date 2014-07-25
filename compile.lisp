3687e6021cdcef3d604d050a43b0f29a6cfb49ed
arturs <arturs@fern>
2014-07-25 13:55:33 +0300

    compile lambda

--- compile.lisp
+++ compile.lisp
@@ -1,4 +1,4 @@
-(defvar *linker-symbols*)
+(defvar *linker-symbols* nil)
 
 (defun el-num (var frame)
   (- (length frame) (length (member var frame))))
@@ -9,7 +9,7 @@
 	(t (find-symbol-lm var (rest env) (1+ depth)))))
 
 (defun is-arithmetic (exp)
-  (member (first exp) '(+ - / *)))
+  (and (consp exp) (member (first exp) '(+ - / *))))
 
 (defun get-arithmetic-opcode (op)
   (list (list (cdr (assoc op '((+ . add) (- . sub) (* . mul) (/ . div)))))))
@@ -32,9 +32,27 @@
 	  `((ldf ,(first exp))
 	    (ap ,(length (rest exp))))))
 
+(defun is-special (exp sym)
+  (and (consp exp) (eq (first exp) sym)))
+
+(defun compile-lambda-body (def env)
+  (compile-lm (second def) (cons (first def) env)))
+
+(defun compile-function (sym def env)
+  (cond ((not (listp (first def))) (error "invalid lambda list ~A" def))
+	((not (= 2 (length def))) (error "malformed lambda body ~A" def))
+	(t (push (cons sym (append (compile-lambda-body def env) '((rtn))))
+		 *linker-symbols*))))
+
+(defun compile-lambda (exp env)
+  (let ((lambda-sym (gensym)))
+    (compile-function lambda-sym (rest exp) env)
+    `((ldf ,lambda-sym))))
+
 (defun compile-lm (exp env)
   (cond ((numberp exp) `((ldc ,exp)))
 	((symbolp exp) (list (find-symbol-lm exp env)))
 	((is-arithmetic exp) (compile-arithmetic exp env))
+	((is-special exp 'lambda) (compile-lambda exp env))
 	((is-application exp) (apply-defined exp env))
 	(t (error "bad expression ~A" exp))))
