a3e5409301d3efb0441ff98df69fcf1f1f381b11
arturs <arturs@fern>
2014-07-25 18:03:33 +0300

    more ugly clockwork

--- clockwork.lisp
+++ clockwork.lisp
@@ -1,8 +1,13 @@
+(defun nth (list n)
+  (if (= n 0)
+      (car list)
+      (nth (cdr list) (- n 1))))
+
 (defun second (list)
-  (car (cdr list)))
+  (nth list 1))
 
 (defun third (list)
-  (car (cdr (cdr list))))
+  (nth list 2))
 
 (defun lambda-man-state (world)
   (second world))
@@ -13,14 +18,36 @@
 (defun lambda-man-direction (world)
   (third (lambda-man-state world)))
 
-(defun advance-state (state)
-  (if (= state 3) 0 (+ state 1))) 
+(defun map-elt (pos map)
+  (nth (nth map (cdr pos)) (car pos)))
 
-(defun new-state (old-pos new-pos state)
-  (if (= (car old-pos) (car new-pos))
-      (if (= (cdr old-pos) (cdr new-pos))
-	  (advance-state state)
-	  state)
+(defun next-pos (pos dir)
+  (if (= dir 0)
+      (cons (car pos) (- (cdr pos) 1))
+      (if (= dir 1)
+	  (cons (+ 1 (car pos)) (cdr pos))
+	  (if (= dir 2)
+	      (cons (car pos) (+ (cdr pos) 1))
+	      (cons (- (car pos) 1) (cdr pos))))))
+
+(defun advance-state (pos map state)
+  (if (= 2 (map-elt (next-pos pos 0) map))
+      0
+      (if (= 2 (map-elt (next-pos pos 1) map))
+	  1
+	  (if (= 2 (map-elt (next-pos pos 2) map))
+	      2
+	      (if (= 2 (map-elt (next-pos pos 3) map))
+		  3
+		  (if (= state 3) 0 (+ state 1)))))))
+
+(defun and (a b)
+  (* a b))
+
+(defun new-state (old-pos new-pos map state)
+  (if (and (= (car old-pos) (car new-pos))
+	   (= (cdr old-pos) (cdr new-pos)))
+      (advance-state new-pos map state)
       state))
 
 (defun main ()
@@ -29,4 +56,5 @@
 	  (cons (lambda-man-pos world)
 		(new-state old-pos
 			   (lambda-man-pos world)
+			   (car world)
 			   (lambda-man-direction world))))))
