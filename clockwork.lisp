1a08f188659ec11aba819c6e396d3ddc492f8e9f
arturs <arturs@fern>
2014-07-25 18:30:07 +0300

    clean up next-pos

--- clockwork.lisp
+++ clockwork.lisp
@@ -21,14 +21,21 @@
 (defun map-elt (pos map)
   (nth (nth map (cdr pos)) (car pos)))
 
+(defun add-pos (pos1 pos2)
+  (cons (+ (car pos1) (car pos2))
+	(+ (cdr pos1) (cdr pos2))))
+
+(defun list4 (a b c d)
+  (cons a (cons b (cons c (cons d 0)))))
+
+(defun dir-list ()
+  (list4 (cons  0 -1)
+	 (cons  1  0)
+	 (cons  0  1)
+	 (cons -1  0)))
+
 (defun next-pos (pos dir)
-  (if (= dir 0)
-      (cons (car pos) (- (cdr pos) 1))
-      (if (= dir 1)
-	  (cons (+ 1 (car pos)) (cdr pos))
-	  (if (= dir 2)
-	      (cons (car pos) (+ (cdr pos) 1))
-	      (cons (- (car pos) 1) (cdr pos))))))
+  (add-pos pos (nth (dir-list) dir)))
 
 (defun advance-state (pos map state)
   (if (= 2 (map-elt (next-pos pos 0) map))
