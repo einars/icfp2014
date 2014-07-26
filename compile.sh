5ac2be0c71315c65694e04ff5ecdae774900f51f
arturs <arturs@fern>
2014-07-26 05:15:02 +0300

    compile option without libs

--- compile.sh
+++ compile.sh
@@ -2,6 +2,9 @@
 
 if [ -e "$1" ]; then
     cat lib.lisp game.lisp $1 > /tmp/tmp.lisp
+    if [ "$2" == "nolibs" ]; then
+	cp $1 /tmp/tmp.lisp
+    fi
     sbcl --noinform --load compile.lisp \
 	--eval \(compile-program\ \"/tmp/tmp.lisp\"\) \
 	--eval "(quit)" 2> /dev/null
